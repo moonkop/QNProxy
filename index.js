9cb8848ee6b01ed9ef6a6c7950040a5e1f70a3e3
moonkop <moonkop@qq.com>
2019-12-02 18:15:30 +0800

    add LOG category

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const WebSocket = require('ws');//引入模块
 let requestPool = {};
 let requestId = 0;
 let buildRequest=(clientId,request)=>{
-    let obj = {client: clientId,id: requestId++,request};
+    let obj = {client: clientId,id: requestId++,request:JSON.stringify(request)};
     requestPool[obj.id] = obj;
     return obj;
 }
@@ -18,25 +18,30 @@ wsClient.on('connection',
         currentClientID++
         wsClients[clientId] = ws;
         console.log(' wsClient connected',clientId);
-        ws.on('message',function incoming(message){
-            console.log('wsClient: %s',message);
-            let request = buildRequest(clientId,message);
+        ws.on('message',function incoming(req){
+            req = JSON.parse(req);
+            if (req.category == 'LOG'){
+                console[req.type].apply(null,req.content)
+                return;
+            }
+            console.log('wsClient:',req);
+            let request = buildRequest(clientId,req);
             if (wsServerInst){
                 wsServerInst.send(JSON.stringify(request));
             }else{
                 console.log('server 未连接');
             }
         });
-    });
+    }
+);
 
 wsServer.on('connection',function connection(ws){
     wsServerInst = ws;
     console.log('wsServer connected');
     ws.on('message',function incoming(message){
-        console.log('wsServer: incoming');
         message = JSON.parse(message);
+        console.log('wsServer: incoming',message);
         wsClients[message.client].send(JSON.stringify(message));
     });
 });
-
 
